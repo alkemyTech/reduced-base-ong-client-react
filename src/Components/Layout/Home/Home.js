3bb031aff4e0c62a676546ae6011e61488101edf
Dario Velez Roman <dvelezroman@gmail.com>
2021-12-08 20:37:40 -0300

    SDK delete

---
+++ src/Components/Layout/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Header/Header";
+import Footer from "../../Footer/Footer";
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <h1>HOME</h1>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
